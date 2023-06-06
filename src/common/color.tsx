9c0c7df74d6b0b6ef30a88d6a9bdf6188ab597fc
CaoThang <thangly2k1@gmail.com>
2023-06-06 10:16:40 +0700

    set up color app

---
+++ src/common/color.tsx
@@ -0,0 +1,22 @@
+enum colorApp {
+    RED = "#ff0000",
+    YELLOW = "#ffff00",
+    GREY = "#CFCFCF",
+    BLUE = "#6495ed",
+    GREEN = "#008000",
+    BLACK = "#000000",
+    WHITE = "#fff",
+    GHOSTWHITE = "#f8f8ff",
+    LIGHTGREY = "#d3d3d3",
+    ORANGE = "#ffa500",
+    DARKORANGE = '#FC7721',
+    DRAKGREY = '#606060',
+    OPACITYWHITE = 'rgba(255, 255, 255, 0.2)',
+    OPACITYBLACK = 'rgba(0, 0, 0, 0.2)',
+}
+
+export const bgColor = {
+    ORANGECORAL: ['#FC7721', '#FD944F'],
+}
+
+export default { colorApp, bgColor }
