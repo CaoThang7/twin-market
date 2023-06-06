0b51b4f8b208f319820c26e3398cb4e1c06eb49f
CaoThang <thangly2k1@gmail.com>
2023-06-06 08:46:55 +0700

    babel plugin module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,39 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json",
+          ],
+          alias: {
+            "@screens": "./src/screens",
+            "@containers": "./src/containers",
+            "@components": "./src/components",
+            "@routes": "./src/routes",
+            "@styles": "./src/styles",
+            "@common": "./src/common",
+            "@navigation": "./src/navigation",
+            "@utils": "./src/utils",
+            "@assets": "./src/assets",
+            "@hooks": "./src/hooks",
+            "@redux": "./src/redux",
+            "@services": "./src/services",
+          },
+        },
+      ],
+    ],
   };
 };
