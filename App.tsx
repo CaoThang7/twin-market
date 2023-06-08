cfda49cbb5095522596eb174a0d6f096dbbd241f
CaoThang <thangly2k1@gmail.com>
2023-06-08 10:35:31 +0700

    #1 ~ using-provider- redux ~

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from "react"
-import { NavigationContainer } from "@react-navigation/native"
 import Router from "./src/navigation/rootSwitch"
+import store from "./src/redux/store"
+import { Provider } from "react-redux"
 
 const App: React.FC = (): JSX.Element => {
   return (
-    <NavigationContainer>
+    <Provider store={store}>
       <Router />
-    </NavigationContainer>
+    </Provider>
   )
 }
 
